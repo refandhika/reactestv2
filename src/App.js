9701acdd06041a8a93c17041b5daec0edc78e7b2
Refa Andhika <refa.andhika@shopee.com>
2019-10-16 10:52:07 +0700

    Initial init

--- src/App.js
+++ src/App.js
@@ -6,19 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	<div className="App-brand">
+	  <img src={logo} className="App-logo" alt="logo" />
+	</div>
       </header>
+      <div className="App-content">
+	<aside className="App-side">
+	
+	</aside>
+	<main className="App-main">
+	</main>
+      </div>
     </div>
   );
 }
