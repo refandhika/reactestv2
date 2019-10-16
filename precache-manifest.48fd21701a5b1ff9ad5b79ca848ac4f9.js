2b027ee4f7e7ed66db3ba24182a9c3d3539f0b8a
Refa Andhika <refa.andhika@shopee.com>
2019-10-16 14:34:24 +0700

    Updates

---
+++ precache-manifest.48fd21701a5b1ff9ad5b79ca848ac4f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389a4e4c11288402056c393602f67ae2",
+    "url": "/reactestv2/index.html"
+  },
+  {
+    "revision": "d87883d15b5562755af9",
+    "url": "/reactestv2/static/css/main.e927e635.chunk.css"
+  },
+  {
+    "revision": "c547285e1557d5237627",
+    "url": "/reactestv2/static/js/2.cc7b10ad.chunk.js"
+  },
+  {
+    "revision": "d87883d15b5562755af9",
+    "url": "/reactestv2/static/js/main.02c8a49c.chunk.js"
+  },
+  {
+    "revision": "afbe78f4fb7aef01a690",
+    "url": "/reactestv2/static/js/runtime-main.c28681a7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactestv2/static/media/logo.25bf045c.svg"
+  }
+]);
